268d50dfd34e944f7189094cac5de43d70607edf
Uncontained0 <uncontained0@gmail.com>
2023-05-09 11:51:03 -0500

    fix publish issues

--- publish.js
+++ publish.js
@@ -1,12 +1,12 @@
-import { exec } from 'child_process';
-import { promisify } from 'util';
-import { readdir } from 'fs/promises';
+let exec = require('child_process').exec;
+let promisify = require('util').promisify;
+let readdir = promisify(require('fs').readdir);
 
 const execAsync = promisify(exec);
 
 readdir('./libs').then(async (files) => {
 	for (const file of files) {
-		execAsync(`wally publish --project-path ./libs/${file}`).then((res) => {
+		await execAsync(`wally publish --project-path ./libs/${file}`).then((res) => {
 			console.log(res.stdout);
 		});
 	}
