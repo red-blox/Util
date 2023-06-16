32a8b52b7f378b52532394001750a8339d388910
Magic <64741104+Mark-Marks@users.noreply.github.com>
2023-06-16 16:57:36 +0200

    Update Collection.d.ts

--- libs/Collection/Collection.d.ts
+++ libs/Collection/Collection.d.ts
@@ -1,1 +1,23 @@
-export function Collection(Tag: string, Start: (Instance: Instance) => () => void | void): () => void;
+/**
+ * Calls all cleanup functions and stops the collection.
+ */
+declare function Stop(): void;
+
+/**
+ * Handles instance addition and removal from collections.
+ * ```ts
+ * const Stop = Collection("Tag", (Instance: Instance) => {
+ *     print("Tag added");
+ *     return () => {
+ *         print("Tag removed");
+ *     }
+ * });
+ *
+ * // After 10 seconds calls all cleanup functions and stops the collection.
+ * task.wait(10);
+ * Stop();
+ * ```
+ * @param Tag Tag
+ * @param Start Callback, called for every instance that has the tag & any that have it added
+ */
+export function Collection(Tag: string, Start: (Instance: Instance) => () => void | void): typeof Stop;
