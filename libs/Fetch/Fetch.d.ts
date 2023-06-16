e523903046c01d4f7903e4a8d09fc4447db981f8
Magic <64741104+Mark-Marks@users.noreply.github.com>
2023-06-17 01:25:50 +0200

    Update Fetch.d.ts

--- libs/Fetch/Fetch.d.ts
+++ libs/Fetch/Fetch.d.ts
@@ -1,39 +1,51 @@
-import { promise } from "@redblox/promise";
-
-type Response = {
-	Body: string;
-	Headers: { [key: string]: string };
-	Status: number;
-	StatusText: string;
-	Ok: boolean;
-	Url: string;
-
-	Json(): promise<unknown, unknown>;
-};
-
-type Options = {
-	Method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "HEAD" | "OPTIONS" | "CONNECT" | "TRACE";
-	Headers?: { [key: string]: string };
-	Body?: string | { [key: string]: unknown };
-};
+type BinItem = Instance | RBXScriptConnection | (() => void);
 
 /**
- * A Promise based HTTP request utility very similar to fetch.
- * This utility takes a resource and an `Options` table and returns a Promise that resolves to a response object. The request options contain the method, any headers, and the body of the request. If you pass a table as the body, it will be encoded as JSON. The response object contains the body, headers, status code, status text, and the URL of the request. The response object also contains a `Json` method that returns a Promise that resolves to the JSON encoded body.
+ * Adds item to Bin.
  * ```ts
- * Fetch("https://api.quotable.io/random").Then((Response) => {
- *     return $tuple(Response.Json());
- * }).Then((Json: { [key: string]: unknown }) => {
- *     print(`${Json.author} once said '${Json.content}'`);
+ * const Part: Part = Add(new Instance("Part"));
+ *
+ * Add(Part.Touched.Connect(() => {
+ *     print("Touched!");
+ * }));
+ *
+ * Add(() => {
+ *     print("Emptying bin!");
  * });
  * ```
- * @param Resource URL to fetch
- * @param Options Request options
+ * @param Item
  */
-declare function Fetch(Resource: string, Options?: Options): promise<Response, unknown>;
+declare function Add<T extends BinItem>(Item: T): T;
 
-declare namespace Fetch {
-	export { Response, Options };
+/**
+ * Empties the Bin - destroys added instances, disconnects added connections, calls added functions.
+ */
+declare function Empty(): void;
+
+/**
+ * Manages cleanup for objects that cannot be garbage collected.
+ * This function creates a Bin, and returns two functions. The first can be used to add items to the bin, and the second can be used to "empty" the bin,
+ * destroying all the items in it.
+ * ```ts
+ * const [Add, Empty] = Bin();
+ *
+ * const Part: Part = Add(new Instance("Part"));
+ *
+ * Add(Part.Touched.Connect(() => {
+ *     print("Touched!");
+ * }));
+ *
+ * Add(() => {
+ *     print("Emptying bin!");
+ * });
+ *
+ * Empty()
+ * ```
+ */
+declare function Bin(): LuaTuple<[typeof Add, typeof Empty]>;
+
+declare namespace Bin {
+    export { BinItem };
 }
 
-export = Fetch;
+export = Bin;
