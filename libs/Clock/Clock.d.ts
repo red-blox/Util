0d7f9ce9658cf8de77bae4520bdbbbbe9ebc106b
Magic <64741104+Mark-Marks@users.noreply.github.com>
2023-06-16 16:59:26 +0200

    Update Clock.d.ts

--- libs/Clock/Clock.d.ts
+++ libs/Clock/Clock.d.ts
@@ -1,5 +1,11 @@
 /**
- * Creates a new Clock. The returned function can be called to stop it.
+ * Stops the clock.
+ */
+declare function Stop(): void;
+
+/**
+ * Calls a function on consistent intervals.
+ * This function will call the given callback every `Interval` seconds. It returns a function that can be called to stop the clock.
  * ```ts
  * const Stop = Clock(1, () => {
  *     print("Hello!");
@@ -8,7 +14,7 @@
  * // Prints "Hello!" every second for 5 seconds.
  * Stop();
  * ```
- * @param Interval Every how many seconds the callback should be called
+ * @param Interval Interval
  * @param Callback Callback
  */
-export function Clock(Interval: number, Callback: () => void): () => void;
+export function Clock(Interval: number, Callback: () => void): typeof Stop;
