a17cafaa5bfc6d246c9076e48e02be50883ba123
Magic <64741104+Mark-Marks@users.noreply.github.com>
2023-06-16 23:25:59 +0200

    Docs for Spawn

--- libs/Spawn/Spawn.d.ts
+++ libs/Spawn/Spawn.d.ts
@@ -1,1 +1,17 @@
+/**
+ * A shared "fast spawn" function that reuses threads.
+ * This utility is used to spawn a function in a new thread. It is similar to `task.spawn`, but doesn't return a thread, and instead reuses a thread if possible. This has been benchmarked many times to produce a significant performance increase.
+ * ```ts
+ * Spawn(() => {
+ *     while (true) {
+ *         print("Hello, World!");
+ *         task.wait(1);
+ *     }
+ * });
+ *
+ * Spawn<string>(print, "Hello", "World!");
+ * ```
+ * @param Callback
+ * @param Args
+ */
 export function Spawn<T>(Callback: (...Args: T[]) => void, ...Args: T[]): void;
