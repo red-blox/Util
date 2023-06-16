e936bcf9f6cf50bad91bc2243e8f48061111f149
Magic <64741104+Mark-Marks@users.noreply.github.com>
2023-06-16 15:09:20 +0200

    Create Signal.d.ts

---
+++ libs/Signal/Signal.d.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import promise from "../red-blox_promise@1.0.0/promise/Promise";
+
+export type SignalNode<T> = {
+	Next?: SignalNode<T>;
+	Callback: (Value: T) => void;
+};
+
+export type Signal<T> = {
+	Root?: SignalNode<T>;
+
+	Connect(Callback: (...Args: T[]) => void): () => void;
+	Wait(): promise<any, any>;
+	Fire(): (...Args: T[]) => void;
+	DisconnectAll(): void;
+};
+
+export default function CreateSignal<T>(): Signal<T>;
