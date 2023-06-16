462315f84a016aa5addbbef59014cd8adaf0d631
Magic <64741104+Mark-Marks@users.noreply.github.com>
2023-06-16 16:39:24 +0200

    Update Bin.d.ts
    Comments

--- libs/Bin/Bin.d.ts
+++ libs/Bin/Bin.d.ts
@@ -1,6 +1,24 @@
 export type BinItem = Instance | RBXScriptConnection | (() => unknown);
 
+/**
+ * Adds item to Bin.
+ * ```ts
+ * Add<RBXScriptConnection>(Heartbeat);
+ * ```
+ * @param Item
+ */
 declare function Add<T>(Item: T & BinItem): T & BinItem;
+
+/**
+ * Empties Bin - destroys instances, disconnects connections, calls functions.
+ */
 declare function Empty(): void;
 
+/**
+ * Creates a new Bin object.
+ * ```ts
+ * import Bin from "?";
+ * const [Add, Empty] = Bin();
+ * ```
+ */
 export default function Bin(): LuaTuple<[typeof Add, typeof Empty]>;
