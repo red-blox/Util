034478c9775ab05ff5ac4aca13036b6407735fe9
Magic <64741104+Mark-Marks@users.noreply.github.com>
2023-06-16 17:06:15 +0200

    Update Bin.d.ts

--- libs/Bin/Bin.d.ts
+++ libs/Bin/Bin.d.ts
@@ -3,21 +3,43 @@ export type BinItem = Instance | RBXScriptConnection | (() => unknown);
 /**
  * Adds item to Bin.
  * ```ts
- * Add<RBXScriptConnection>(Heartbeat);
+ * const Part: Part = Add<Part>(new Instance("Part"));
+ *
+ * Add<RBXScriptConnection>(Part.Touched.Connect(() => {
+ *     print("Touched!");
+ * }));
+ *
+ * Add<Callback>(() => {
+ *     print("Emptying bin!");
+ * });
  * ```
  * @param Item
  */
 declare function Add<T>(Item: T & BinItem): T & BinItem;
 
 /**
- * Empties Bin - destroys instances, disconnects connections, calls functions.
+ * Empties the Bin - destroys added instances, disconnects added connections, calls added functions.
  */
 declare function Empty(): void;
 
 /**
- * Creates a new Bin object.
+ * Manages cleanup for objects that cannot be garbage collected.
+ * This function creates a Bin, and returns two functions. The first can be used to add items to the bin, and the second can be used to "empty" the bin,
+ * destroying all the items in it.
  * ```ts
  * const [Add, Empty] = Bin();
+ *
+ * const Part: Part = Add<Part>(new Instance("Part"));
+ *
+ * Add<RBXScriptConnection>(Part.Touched.Connect(() => {
+ *     print("Touched!");
+ * }));
+ *
+ * Add<Callback>(() => {
+ *     print("Emptying bin!");
+ * });
+ *
+ * Empty()
  * ```
  */
 export function Bin(): LuaTuple<[typeof Add, typeof Empty]>;
