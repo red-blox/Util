9ab545d629933663088cfe77d86a90a222a74b03
Magic <64741104+Mark-Marks@users.noreply.github.com>
2023-06-16 18:15:32 +0200

    Update Signal.d.ts

--- libs/Signal/Signal.d.ts
+++ libs/Signal/Signal.d.ts
@@ -6,13 +6,68 @@ export type SignalNode<T> = {
 	Callback: (Value: T) => void;
 };
 
+/**
+ * Disconnects from the Signal.
+ */
+declare function Disconnect(): void;
+
 export type Signal<T> = {
 	Root?: SignalNode<T>;
 
-	Connect(Callback: (...Args: T[]) => void): () => void;
+	/**
+	 * Connects the listener to the Signal and returns a function that can be used to disconnect the listener from the signal. Listeners are passed all fired values.
+	 *
+	 * `Connect` passes a return function to allow easy use with utilities such as Bin, and to reduce bloat.
+	 * ```ts
+	 * const Disconnect = Signal.Connect((...Values: unknown[]) => {
+	 *     print(...Values);
+	 * });
+	 *
+	 * Signal.Fire("Hello, World!"); // "Hello, World!"
+	 * Disconnect();
+	 * Signal.Fire("Hello, World!"); // No output.
+	 * ```
+	 * @param Callback
+	 */
+	Connect(Callback: (...Args: T[]) => void): typeof Disconnect;
+
+	/**
+	 * Returns a Promise that resolves when the Signal is next fired. The Promise will resolve with all values fired.
+	 * ```ts
+	 * Signal.Wait().Then(print).Await();
+	 * ```
+	 */
 	Wait(): promise<any, any>;
-	Fire(): (...Args: T[]) => void;
+
+	/**
+	 * Fires the passed values to all connected listeners.
+	 * ```ts
+	 * Signal.Fire("Hello, World!");
+	 *
+	 * // You can also fire multiple values
+	 * Signal.Fire("Hello", "World!", 15, true);
+	 * ```
+	 */
+	Fire(...Args: T[]): void;
+
+	/**
+	 * Disconnects all listeners from the Signal, enabling it to be garbage collected.
+	 */
 	DisconnectAll(): void;
 };
 
-export default function CreateSignal<T>(): Signal<T>;
+/**
+ * A signal implementation without connection objects.
+ *
+ * Creates & returns a new Signal object, which can be used to fire any number of events and listen to them in any number of callbacks.
+ *
+ * ```ts
+ * const LogSignal = Signal<unknown>();
+ *
+ * LogSignal.Connect((...Values: unknown[]) => {
+ *     print(...Values);
+ * });
+ *
+ * LogSignal.Fire("Hello, World!");
+ */
+export function Signal<T>(): Signal<T>;
