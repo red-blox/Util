da6a035259d0511bf9de739d88f51d93b2edb5df
Magic <64741104+Mark-Marks@users.noreply.github.com>
2023-06-16 15:29:21 +0200

    Create Fetch.d.ts

---
+++ libs/Fetch/Fetch.d.ts
@@ -0,0 +1,20 @@
+import promise from "../../red-blox_promise@1.0.0/promise/Promise";
+
+export type Response = {
+	Body: string;
+	Headers: { [key: string]: string };
+	Status: number;
+	StatusText: string;
+	Ok: boolean;
+	Url: string;
+
+	Json(): unknown;
+};
+
+export type Options = {
+	Method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "HEAD" | "OPTIONS" | "CONNECT" | "TRACE";
+	Headers?: { [key: string]: string };
+	Body?: string | { [key: string]: unknown };
+};
+
+export default function Fetch(Resource: string, Options?: Options): promise<Response, unknown>;
