bd8aa6225ed4cb84ebadbebebb97482c8074d1de
jackdotink <jackdotink@gmail.com>
2023-09-02 19:05:04 -0500

    make node stop yelling at me [no ci]

--- publish.js
+++ publish.js
@@ -1,10 +1,12 @@
-let exec = require('child_process').exec;
-let promisify = require('util').promisify;
-let readdir = promisify(require('fs').readdir);
+import { exec } from 'child_process';
+import { promisify } from 'util';
+import { readdir } from 'fs';
+
+const preaddir = promisify(readdir);
 
 const execAsync = promisify(exec);
 
-readdir('./libs').then(async (files) => {
+preaddir('./libs').then(async (files) => {
 	for (const file of files) {
 		await execAsync(`wally publish --project-path ./libs/${file}`).then((res) => {
 			console.log(res.stdout);
